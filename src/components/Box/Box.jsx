07683038cc2f065aa004351b86aedec31e6716eb
Quentin MACHARD <quentin.machard@gmail.com>
2020-02-08 18:02:00 +0100

    :sparkles: Cleanup code

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,20 +1,21 @@
-    import React, { Component } from 'react'
-    
-    const Box = ({boxInformation}) =>{
-        return (
-            <>
-                <div class="toolsContent">
-                    <div class="code">
-                        <h2>Code:</h2>
-                        <div>{boxInformation.code}</div>
-                    </div>
-                    <div class="tools">
-                        {boxInformation.tools.map(tool => {
-                            return ( <div>{tool.name}</div>)
-                        })}
-                    </div>
-                </div>
-            </>
-        )
-    }
-    
+import React from 'react'
+
+const Box = ({boxInformation}) => {
+  return (
+    <>
+      <div className="toolsContent">
+        <div className="code">
+          <h2>Code:</h2>
+          <div>{boxInformation.code}</div>
+        </div>
+        <div className="tools">
+          {boxInformation.tools.map(tool => {
+            return (<div>{tool.name}</div>)
+          })}
+        </div>
+      </div>
+    </>
+  )
+};
+
+export {Box};
