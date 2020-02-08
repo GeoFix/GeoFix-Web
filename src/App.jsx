8a54d6acc1fe642e3b39366e51f4b50ebc9af27d
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2020-02-08 19:04:11 +0100

    :recycle: Put Map in a container

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import { Map } from "./components/Map";
-import { useBoxes } from "./hooks/useBoxes";
+import { MapLayout } from "./containers/MapLayout";
 
 import './App.css';
 
 export default function App() {
-  const { isLoading, boxes } = useBoxes();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <Router>
       <Switch>
         <Route path="/" exact>
-          <Map markers={boxes.map(({ geopoint, id }) => ({
-            lat: geopoint.latitude,
-            long: geopoint.longitude,
-            id,
-          }))} />
+          <MapLayout />
         </Route>
         <Route path="/create">
           <p>bla</p>
