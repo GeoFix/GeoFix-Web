89701a8681a3d61ea3cd28921041eb98323f6c5c
charles hardy <hardy.charles35340@gmail.com>
2020-02-08 17:51:03 +0100

    adding box component

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,20 @@
+    import React, { Component } from 'react'
+    
+    const Box = ({boxInformation}) =>{
+        return (
+            <>
+                <div class="toolsContent">
+                    <div class="code">
+                        <h2>Code:</h2>
+                        <div>{boxInformation.code}</div>
+                    </div>
+                    <div class="tools">
+                        {boxInformation.tools.map(tool => {
+                            return ( <div>{tool}</div>)
+                        })}
+                    </div>
+                </div>
+            </>
+        )
+    }
+    
