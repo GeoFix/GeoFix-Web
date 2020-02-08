6b2e83989b93fedbdbc37e4f9631c1bac5f63f15
Quentin MACHARD <quentin.machard@gmail.com>
2020-02-08 17:50:54 +0100

    :sparkles: Add style on Map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -8,15 +8,17 @@ import OlXYZ from 'ol/source/XYZ';
 import OlTileLayer from 'ol/layer/Tile';
 import OlView from 'ol/View';
 import OlMap from 'ol/Map';
-import { fromLonLat } from 'ol/proj';
+import Point from 'ol/geom/Point';
+import Feature from 'ol/Feature';
+import { fromLonLat, transform } from 'ol/proj';
 import VectorSource from 'ol/source/Vector';
 import {Vector as VectorLayer} from 'ol/layer';
 
 /**
  * Map Component
  */
-const Map = ({ markers }) => {
-  const [ map, setMap ] = useState();
+const Map = ({ markers, onMarkerClick }) => {
+  const [ vectorSource, setVectorSource ] = useState();
 
   useEffect(() => {
     // Create open street map layer
@@ -42,21 +44,70 @@ const Map = ({ markers }) => {
     });
 
     //Init map
-    setMap(new OlMap({
+    const map = new OlMap({
       target : "map",
       layers:[layer, newPinLayer],
       view : view
-    }));
-  }, []);
+    });
+
+    let first = true;
+
+    map.on('singleclick', event => {
+      const feature = map.forEachFeatureAtPixel(event.pixel, feature => {
+        return feature;
+      });
+
+      var latLong = transform(event.coordinate, 'EPSG:3857', 'EPSG:4326');
+      // var lat     = latLong[1];
+      // var long    = latLong[0];
+
+      console.log(latLong);
+
+      if (!feature) {
+        return;
+      }
+
+      const [lat, long] = transform(feature.getGeometry().getCoordinates(), 'EPSG:3857', 'EPSG:4326');
+
+      onMarkerClick({
+        id: feature.get('id'),
+        lat,
+        long,
+      });
+    });
+
+    map.on('rendercomplete',  () => {
+      if(first) {
+        first = false;
+
+        setVectorSource(vectorSource);
+      }
+    });
+  }, [setVectorSource, onMarkerClick]);
 
   useEffect(() => {
+    if (!vectorSource) {
+      return;
+    }
 
-  }, [markers, map]);
+    vectorSource.clear();
+
+    markers.forEach(({lat, long, id}) => {
+      let iconFeature = new Feature({
+        id,
+        geometry: new Point(fromLonLat([long, lat])),
+      });
+
+      vectorSource.addFeature(iconFeature);
+    });
+
+    // iconFeature.setStyle(this.iconStyle);
+  }, [markers, vectorSource]);
 
   return (
     <>
       <div id="map"/>
-      <div className="copyright">
+      <div className="map_copyright">
         &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors,
         Tiles style by <a href="https://www.hotosm.org/" target="_blank" rel="noopener noreferrer" >Humanitarian OpenStreetMap Team</a>
         hosted by <a href="https://openstreetmap.fr/" target="_blank" rel="noopener noreferrer" >OpenStreetMap France</a>
