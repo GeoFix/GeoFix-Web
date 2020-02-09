c371d7964d532b65eb21831b250f96a385d916ac
Quentin MACHARD <quentin.machard@gmail.com>
2020-02-09 13:51:03 +0100

    :sparkles: Enable to display current user position

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/functions';
+import 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "**HIDDEN_DATA**",
