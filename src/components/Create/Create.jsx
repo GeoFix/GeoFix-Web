6be3ab14f310e1dd38e9c13cb8f73e6a7f592cc3
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2020-02-09 13:07:22 +0100

    :sparkles: Add tools

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -1,18 +1,36 @@
 import React, { Fragment } from 'react'
 import { useForm } from 'react-hook-form'
+import { useHistory } from "react-router-dom";
+import firebase from "../../utils/firebase";
 import { Button, ButtonLink, InputField } from '../../UIElements';
+import { useTools } from "../../hooks/useTools";
+import { createBox } from "../../hooks/createBox";
 import { Map } from "../Map";
 
 import './Create.css'
 
 export function Create() {
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => { console.log(data) };
+  const { tools, isLoading } = useTools();
+  const history = useHistory();
+
   const position = {
     coords: {
       latitude: 48.110182,
       longitude: -1.678994,
     }
+  };
+
+  const onSubmit = data => {
+    const newTools = tools.map(({ id }) => (
+      {
+        tool: firebase.firestore().collection('tools').doc(id),
+        count: data[id],
+      }
+    ));
+
+    createBox({ name: data.name, tools: newTools })
+    history.push('/')
   };
 
   return (
@@ -26,23 +44,35 @@ export function Create() {
           <InputField label="Nom de la boîte" name="name" inputRef={register({ required: true })} error={errors.name} />
 
           <p>Position</p>
-          <Map
-            className="create-box-map"
-            position={position}
-            markers={[{
-              lat: position.coords.latitude,
-              long: position.coords.longitude,
-              id: '1234',
-            }]}
-            center={[position.coords.longitude, position.coords.latitude]}
-            controls={false}
-          />
+          <div className="map-container">
+            <Map
+              className="create-box-map"
+              position={position}
+              markers={[{
+                lat: position.coords.latitude,
+                long: position.coords.longitude,
+                id: '1234',
+              }]}
+              center={[position.coords.longitude, position.coords.latitude]}
+              controls={false}
+            />
+          </div>
         </fieldset>
 
         <fieldset>
           <legend>Outils disponibles</legend>
 
-          <InputField label="Pompe à vélo" name="pump" inputRef={register} error={errors.pump} type="checkbox" />
+          {tools.map(({ id, name }) => (
+            <InputField
+              label={name}
+              name={id}
+              inputRef={register}
+              error={errors[name]}
+              type="number"
+              defaultValue={0}
+              key={id}
+            />
+          ))}
         </fieldset>
 
         <span className="button-field">
--- src/hooks/createBox.js
+++ src/hooks/createBox.js
@@ -1,34 +1,16 @@
-import {useEffect, useState} from "react";
-
 import firebase from "../utils/firebase";
-import {slugify} from "../utils/format";
 
-export const createBox = (data) => {
+export const createBox = ({ name, tools }) => {
   // const [isLoading, setIsLoading] = useState(true);
-  const id = slugify(data.name);
-  const position = {
-    coords: {
-      latitude: 48.110182,
-      longitude: -1.678994,
-    }
-  };
+  const geopoint = new firebase.firestore.GeoPoint(48.110182, -1.678994);
 
-  useEffect(() => {
-    // setIsLoading(true);
-
-    firebase
-      .ref(`/boxes/${id}`)
-      .set({
-        id,
-        name: data.name,
-        geopoint: {
-          lat: position.coords.latitude,
-          long: position.coords.longitude,
-        },
-        tools: data.tools,
-      })
-      .then(test => console.log(test), console.error.bind(console));
-  }, []);
-
-  // return {isLoading, boxes};
+  return firebase
+    .firestore()
+    .collection('boxes')
+    .add({
+      name,
+      geopoint,
+      tools,
+      code: 1234,
+    })
 };
