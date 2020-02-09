59ed135e79a964cc32f4f1d9412e57dde21e149d
charles hardy <hardy.charles35340@gmail.com>
2020-02-09 10:04:59 +0100

    adding box style

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Box.scss';
+import cross from '../../assets/cross.svg';
 
 const Box = ({boxInformation}) => {
   return (
     <>
       <div className="toolsContent">
+        <div className="absoluteCross"><img className="cross" src={cross}></img></div>
         <div className="code">
           <h2>Code:</h2>
           <div>{boxInformation.code}</div>
         </div>
         <div className="tools">
           {boxInformation.tools.map((tool, index) => (
-            <div key={`tool-${index}`}>{tool.name}</div>
+            <div key={`tool-${index}`}><p>Tool icon: {tool.icon}</p><p>Tool: {tool.name}</p></div>
           ))}
         </div>
       </div>
