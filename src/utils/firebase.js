e8c55f0a7f1aa703ebbcbe78c4e4803c6eed40de
Quentin MACHARD <quentin.machard@gmail.com>
2020-02-10 22:15:34 +0100

    :wrench: Enable environment configuration for Firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,15 +5,7 @@ import 'firebase/firestore';
 import 'firebase/functions';
 import 'firebase/database';
 
-const firebaseConfig = {
-  apiKey: "**HIDDEN_DATA**",
-  authDomain: "geofix-4e08e.firebaseapp.com",
-  databaseURL: "https://geofix-4e08e.firebaseio.com",
-  projectId: "geofix-4e08e",
-  storageBucket: "geofix-4e08e.appspot.com",
-  messagingSenderId: "619006444915",
-  appId: "**HIDDEN_DATA**"
-};
+import firebaseConfig from '../config/firebase.config';
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
